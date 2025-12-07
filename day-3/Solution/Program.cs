3314383e30c1c97352d6150c8a33e0c1c2b63373
Harrison-Blair <hjblair@me.com>
2025-12-07 16:29:12 -0500

    day-3 Part-1 Soltuion

--- day-3/Solution/Program.cs
+++ day-3/Solution/Program.cs
@@ -8,10 +8,12 @@
             if (!File.Exists(path))
                 throw new FileNotFoundException("File does not exist @ " + path);
 
-            foreach (string line in File.ReadAllLines(path)) {
+            foreach (string line in File.ReadAllLines(path))
+            {
                 List<int> lineInts = new List<int>();
                 char[] charArr = line.ToCharArray();
-                foreach (char c in charArr) {
+                foreach (char c in charArr)
+                {
                     lineInts.Add(c - '0');
                 }
                 ints.Add(lineInts);
@@ -23,21 +25,36 @@
         }
         return ints;
     }
-    
-    static int PartOne(List<List<int>> batteryBanks) {
+
+    static int PartOne(List<List<int>> batteryBanks)
+    {
         int total = 0;
 
-        foreach (List<int> battery in batteryBanks) {
-            
+        foreach (List<int> battery in batteryBanks)
+        {
+            int maxCharge = 0;
+            for (int i = battery.Count() - 2; i >= 0; i--)
+            {
+                for (int j = battery.Count() - 1; j >= 1; j--)
+                {
+                    if (j == i)
+                        break;
+                    if (maxCharge < (battery[i] * 10) + battery[j])
+                        maxCharge = (battery[i] * 10) + battery[j];
+                }
+            }
+            total += maxCharge;
+            Console.WriteLine(maxCharge);
         }
         return total;
     }
 
     public static void Main()
     {
-        string path = "/home/penguin/source/advent-of-code-2025/day-3/example.txt";
-        
-        if (GetIntsFromFile(path) is List<List<int>> batteryBanks) {
+        string path = "/home/penguin/source/advent-of-code-2025/day-3/input.txt";
+
+        if (GetIntsFromFile(path) is List<List<int>> batteryBanks)
+        {
             Console.WriteLine(PartOne(batteryBanks));
         }
     }
