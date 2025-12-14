8c11bef485f2e82a17f20ae7f3c3e3aa73a54e93
Harrison-Blair <hjblair@me.com>
2025-12-13 21:03:22 -0500

    Part 1 completion

--- day-4/PART-1.md
+++ day-4/PART-1.md
@@ -11,7 +11,7 @@ If you can optimize the work the forklifts are doing, maybe they would have time
 The rolls of paper (@) are arranged on a large grid; the Elves even have a helpful diagram (your puzzle input) indicating where everything is located.
 
 For example:
-
+```
 ..@@.@@@@.
 @@@.@.@.@@
 @@@@@.@.@@
@@ -22,11 +22,11 @@ For example:
 @.@@@.@@@@
 .@@@@@@@@.
 @.@.@@@.@.
-
+```
 The forklifts can only access a roll of paper if there are fewer than four rolls of paper in the eight adjacent positions. If you can figure out which rolls of paper the forklifts can access, they'll spend less time looking and more time breaking down the wall to the cafeteria.
 
 In this example, there are 13 rolls of paper that can be accessed by a forklift (marked with x):
-
+```
 ..xx.xx@x.
 x@@.@.@.@@
 @@@@@.x.@@
@@ -37,5 +37,5 @@ x@.@@@@.@x
 x.@@@.@@@@
 .@@@@@@@@.
 x.x.@@@.x.
-
+```
 Consider your complete diagram of the paper roll locations. How many rolls of paper can be accessed by a forklift?
--- day-4/solution.cpp
+++ day-4/solution.cpp
@@ -1,38 +1,73 @@
-#include <string>
-
 #include <fstream>
 #include <iostream>
-#include <list>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-list<list<char>> readFile(string fileName = "example.txt") {
-    list<list<char>> grid;
+vector<vector<char>> readFile(string fileName = "example.txt") {
+  vector<vector<char>> grid;
 
-    fstream fs(fileName, fstream::in);
-    
-    if (!fs.is_open()) {
-        cout << "[ERROR] Failed to open file: " << fileName << endl;
-        return grid;
-    }
+  fstream fs(fileName, fstream::in);
 
-    string input;
-    while (getline(fs, input)) {
-        list<char> line;
-        for (char c : input) {
-            line.push_back(c);
-        }
-        grid.push_back(line);
-    }
-    
-    fs.close();
+  if (!fs.is_open()) {
+    cout << "[ERROR] Failed to open file: " << fileName << endl;
     return grid;
+  }
+
+  string input;
+  while (getline(fs, input)) {
+    vector<char> line;
+    for (char c : input) {
+      line.push_back(c);
+    }
+    grid.push_back(line);
+  }
+
+  fs.close();
+  return grid;
+}
+
+vector<pair<int, int>> getRollLocations(vector<vector<char>> grid) {
+  vector<pair<int, int>> rolls;
+  for (int i = 0; i < grid.size(); i++) {
+    for (int j = 0; j < grid[i].size(); j++) {
+      if (grid[i][j] == '@') rolls.push_back(pair(i, j));  // y, x
+    }
+  }
+  return rolls;
+}
+
+int partOne(vector<vector<char>> grid) {
+  int total = 0;
+  vector<pair<int, int>> rolls = getRollLocations(grid);
+
+  for (auto& roll : rolls) { 
+    int numNeighbors = 0;
+    // Check all 8 possible directions
+    for (int di = -1; di <= 1; di++) {
+      for (int dj = -1; dj <= 1; dj++) {
+        if (di == 0 && dj == 0) continue;  // Skip self
+        int ni = roll.first + di;
+        int nj = roll.second + dj;
+        if (ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size() && grid[ni][nj] == '@') {
+          numNeighbors++;
+        }
+      }
+    }
+    if (numNeighbors < 4) {
+      total++;
+    }
+  }
+  return total;
 }
 
 int main() {
-    string fileName = "input.txt"; //presupposes ./
+  string fileName = "input.txt";
 
-    list<list<char>> grid = readFile(fileName);
+  vector<vector<char>> grid = readFile(fileName);
 
-    return 0;
+  cout << partOne(grid) << endl;
+
+  return 0;
 }
