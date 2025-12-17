663ec232dfd3b3174a8a87f4d299a8e96b3616c6
Harrison-Blair <hjblair@me.com>
2025-12-17 13:11:25 -0500

    add part-1 solution, begining of part-2

---
+++ day-6/NOTES.md
@@ -0,0 +1,6 @@
+# Notes
+
+I should probably make a `cargo.toml` file, but I will not, I wish to suffer (gain knowledge). Ideally this helps to make what cargo does (more than just a package-manager & build tool) painfully apparent (I'll have to do it manually). Also surely this makes getting external libraries a pain, which I do not want to use.
+
+The above has been disregarded, cuz Cargo is cool, and makes this so much easier. And my logic is flawed, as in order to learn as much as possible about Rust I need to learn how to use Cargo.
+
--- day-6/solution/src/main.rs
+++ day-6/solution/src/main.rs
@@ -1,3 +1,71 @@
+use std::fs;
+
+fn read_file(file_name: &str) -> (Vec<Vec<u128>>, Vec<char>) {
+    let mut nums: Vec<Vec<u128>> = Vec::new();
+    let mut ops: Vec<char> = Vec::new();
+
+    let text: String = fs::read_to_string(file_name)
+        .expect("[N] Failed to read from file!");
+
+    for line in text.lines() {
+        let mut row: Vec<u128> = Vec::new();
+        for item in line.split_whitespace() {
+            if let Ok(num) = item.parse::<u128>() {
+                row.push(num);
+            } else {
+                ops.push(item.chars().next().unwrap());
+            }
+        }
+        // Only push non-empty rows (skip the operator line)
+        if !row.is_empty() {
+            nums.push(row);
+        }
+    }
+
+    (nums, ops)
+}
+
+fn read_file_right_left(file_name: &str) -> Vec<Vec<u128>> {
+    let mut nums: Vec<Vec<u128>> = Vec::new();
+
+    let text: String = fs::read_to_string(file_name)
+        .expect("[RL] Failed to read from file!");
+
+    
+
+    nums
+}
+
+fn evaluate_nums(nums: Vec<Vec<u128>>, ops: Vec<char>) -> u128 {
+    let mut total: u128 = 0;
+
+    for i in 0..ops.len() {
+        let mut result = nums[0][i];
+        for row_idx in 1..nums.len() {
+            if ops[i] == '*' {
+                result *= nums[row_idx][i];
+            } else {
+                result += nums[row_idx][i]
+            }
+        }
+        total += result;
+    }
+
+    total
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "day-6/input.txt";
+    println!("Adv-of-Code [DAY-6] | Input File : {file_name}");
+
+    let (nums, ops) = read_file(file_name);
+
+    let p1_solution = evaluate_nums(nums, ops.clone());
+
+    let c_nums = read_file_right_left(file_name);
+
+    let p2_solution: u128 = evaluate_nums(c_nums, ops.clone());
+
+    println!("PART-1 [RESULT] | {p1_solution}");
+    println!("PART-2 [RESULT] | {p2_solution}");
 }
